445061d8da4564874551d9560956ce6491164ad8
Josiah Coad <josiah@coad.net>
2018-03-01 23:29:35 -0600

    removing boostrap for now

--- src/Components/PaperItem.js
+++ src/Components/PaperItem.js
@@ -11,7 +11,7 @@ class PaperItem extends Component {
 
   render() {
     return (
-      <div className="Paper" style={{ "background-color": "lightgray" }}>
+      <div className="Paper jumbotron bg-info" style={{ "backgroundColor": "lightgray" }}>
         <h3>{this.props.Paper.title}</h3>
         <p>{this.props.Paper.category}</p>
         <p>{this.props.Paper.absract}
