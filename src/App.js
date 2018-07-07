86082b8fee7498ea926826c396afaffacfaf450f
Josiah Coad <josiah@coad.net>
2018-07-07 12:43:31 -0700

    made a working dummy example

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import React from 'react';
 import { Grid } from 'react-bootstrap';
 
 import 'Style/index.css';
-import PaperScreening from 'Presentationals/paperScreening/PaperScreening';
+// import PaperScreening from 'Presentationals/paperScreening/PaperScreening';
 import RevNavbar from 'Elements/RevNavbar';
 import { Colors } from 'Constants';
+import RouteExample from './RouteExample';
 
 const App = () => (
     <div style={{ backgroundColor: Colors.REVBACKGROUND, height: '100vh' }}>
         <RevNavbar />
         <Grid fluid>
-            <PaperScreening />
+            <RouteExample />
+            {/* <PaperScreening /> */}
         </Grid>
     </div>
 );
