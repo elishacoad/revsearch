0400244e6ab9a788d52c1af99511af22aa21a253
Josiah Coad <josiah@coad.net>
2018-07-07 10:14:28 -0700

    styling

--- src/app/components/presentationals/navbar/RevNavbarPresentational.js
+++ src/app/components/presentationals/navbar/RevNavbarPresentational.js
@@ -1,32 +1,36 @@
 import React from 'react';
 import { Navbar, MenuItem, NavDropdown, Glyphicon, Nav } from 'react-bootstrap';
 
-import { Colors, RevNavbarEventKeys } from '../../../globals/constants';
+import { RevNavbarEventKeys } from '../../../globals/constants';
 import DownloadPapersModal from '../../containers/navbar/DownloadPapersModal';
 
 const RevNavbarPresentational = props => (
     <div>
         <Navbar
-            style={{ backgroundColor: Colors.REVNAVY }}
+            className="rev-navbar"
             onSelect={props.handleNavSelect}
         >
             <Navbar.Header>
                 <Navbar.Brand>
-                    <a href="#home" style={{ color: 'white' }}>reVsearch</a>
+                    <a href="#home" className="color-white">reVsearch</a>
                 </Navbar.Brand>
             </Navbar.Header>
             <Nav pullRight>
                 <NavDropdown
                     title={
-                        <div style={{ display: 'inline-block' }}>
+                        <div
+                            style={{ display: 'inline-block' }}
+                            className="color-white"
+                        >
                             <Glyphicon
-                                glyph="wrench"
+                                glyph="list-alt"
                                 className="nav-dropdown-icon"
                             />
+                            {' '} Paper Options
                         </div>
                     }
                     id="nav-dropdown-tools"
-                    noCaret
+                    className="color-white"
                 >
                     <MenuItem eventKey={RevNavbarEventKeys.TOOLS.DOWNLOAD}>
                         <Glyphicon glyph="download-alt" />{' '}Download Papers
